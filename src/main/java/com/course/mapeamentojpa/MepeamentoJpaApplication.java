62e29ff755895cc7e10aa6f9dc55ec99ebcad587
Diego Mucheniski <mucheniski@gmail.com>
2019-09-14 00:13:14 -0300

    Primeiro commit

---
+++ src/main/java/com/course/mapeamentojpa/MepeamentoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.course.mapeamentojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepeamentoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MepeamentoJpaApplication.class, args);
+	}
+
+}
