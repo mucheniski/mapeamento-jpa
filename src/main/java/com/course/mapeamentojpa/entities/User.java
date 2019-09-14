55905aec0a8e9bb660172ab5e26c4ad16794ee74
Diego Mucheniski <mucheniski@gmail.com>
2019-09-14 12:40:30 -0300

    Order, Instant, ISO 8601 - Part 1

--- src/main/java/com/course/mapeamentojpa/entities/User.java
+++ src/main/java/com/course/mapeamentojpa/entities/User.java
@@ -1,11 +1,17 @@
 package com.course.mapeamentojpa.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
 	
 	@Id
@@ -16,6 +22,9 @@ public class User {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -65,6 +74,10 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
