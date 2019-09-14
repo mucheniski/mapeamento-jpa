3f9081d7ac8c6962bc7097f7904154498d082bbb
Diego Mucheniski <mucheniski@gmail.com>
2019-09-14 10:19:54 -0300

    H2 database, test profile, JPA

--- src/main/java/com/course/mapeamentojpa/entities/User.java
+++ src/main/java/com/course/mapeamentojpa/entities/User.java
@@ -1,8 +1,17 @@
 package com.course.mapeamentojpa.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+		
 	private String name;
 	private String email;
 	private String phone;
