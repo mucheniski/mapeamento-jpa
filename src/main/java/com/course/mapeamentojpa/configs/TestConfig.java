93e82582685220b75a6569bda5b7d834832c336b
Diego Mucheniski <mucheniski@gmail.com>
2019-09-14 16:06:56 -0300

    Category

--- src/main/java/com/course/mapeamentojpa/configs/TestConfig.java
+++ src/main/java/com/course/mapeamentojpa/configs/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.course.mapeamentojpa.entities.Category;
 import com.course.mapeamentojpa.entities.Order;
 import com.course.mapeamentojpa.entities.User;
 import com.course.mapeamentojpa.enums.OrderStatus;
+import com.course.mapeamentojpa.repositories.CategoryRepository;
 import com.course.mapeamentojpa.repositories.OrderRepository;
 import com.course.mapeamentojpa.repositories.UserRepository;
 
@@ -24,6 +26,10 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -37,6 +43,14 @@ public class TestConfig implements CommandLineRunner {
 		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 		
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+		
+		Category category1 = new Category(null, "Electronics");
+		Category category2 = new Category(null, "Books");
+		Category category3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+		
+		
 	}
 	
 		
---
+++ src/main/java/com/course/mapeamentojpa/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.course.mapeamentojpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.mapeamentojpa.entities.Category;
+import com.course.mapeamentojpa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category.get();
+	}
+	
+}
